906afb19e07e6b660ce1b4ae4b885f3402a05ef4
MartinGuiaDev <marting_dev@hotmail.com>
2025-02-28 09:39:54 -0600

    Modifying paths, dockerfiles and icons

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Conéctate al servidor de Socket.io
-const socket = io("http://localhost:4000", { autoConnect: true });
+const socket = io(process.env.REACT_APP_URL_BACKEND, { autoConnect: true });
 
 socket.on("connect", () => {
   console.log("🔌 Conectado a Socket.io con ID:", socket.id);
